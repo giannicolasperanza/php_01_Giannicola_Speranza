e45e0c41d9b9ab5a943a5459fbf418e225865290
Giannicola Speranza <speranzagiannicola@gmail.com>
2025-04-16 16:53:54 +0200

    secondo esercizio

--- index.php
+++ index.php
@@ -1,27 +1,41 @@
 <?php
 
+// primo esercizio
 
-$integer = 24;
-$float = 5.6;
-$string = 'ciao ciao';
-$bol = true;
+// $integer = 24;
+// $float = 5.6;
+// $string = 'ciao ciao';
+// $bol = true;
 
-var_dump($integer, $float, $string, $bol);
+// var_dump($integer, $float, $string, $bol);
 
-const INTERI  = 10;
-const NUMERICONVIRGOLA = 2.5;
-const STRINGA = 'Ciao sono una stringa';
-const BOOOL = true;
-
-
-
-var_dump(INTERI, NUMERICONVIRGOLA, STRINGA, BOOOL);
+// const INTERI  = 10;
+// const NUMERICONVIRGOLA = 2.5;
+// const STRINGA = 'Ciao sono una stringa';
+// const BOOOL = true;
 
 
 
+// var_dump(INTERI, NUMERICONVIRGOLA, STRINGA, BOOOL);
 
 
+// fine primo esercizio 
 
+// secondo esercizio
+
+$text1 = "Marco";
+$text2 = "hai";
+$text3 = "sete";
+$text4 = "?";
+$text5 = "Perchè";
+$text6 = $text2;
+$text7 = 'bevuto';
+$text8 = "tutto";
+
+echo $text1 . " " . $text2 . " " . $text3 . $text4 . " " . $text5 . " " . $text6 . " " . $text7 . " " . $text8;
+
+
+// fine secondo esercizio
 
 
 
