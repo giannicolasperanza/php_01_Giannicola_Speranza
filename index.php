6393736d7d950fda20cf5053d802f33ab6381397
Giannicola Speranza <speranzagiannicola@gmail.com>
2025-04-16 16:41:09 +0200

    primo esercizio

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+
+$integer = 24;
+$float = 5.6;
+$string = 'ciao ciao';
+$bol = true;
+
+var_dump($integer, $float, $string, $bol);
+
+const INTERI  = 10;
+const NUMERICONVIRGOLA = 2.5;
+const STRINGA = 'Ciao sono una stringa';
+const BOOOL = true;
+
+
+
+var_dump(INTERI, NUMERICONVIRGOLA, STRINGA, BOOOL);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
