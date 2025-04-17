5e4e1d3d62cf6066ba4d7674d6005d4259a1a8f7
Giannicola Speranza <speranzagiannicola@gmail.com>
2025-04-17 12:43:39 +0200

    terzo esercizio

--- index.php
+++ index.php
@@ -23,24 +23,31 @@
 
 // secondo esercizio
 
-$text1 = "Marco";
-$text2 = "hai";
-$text3 = "sete";
-$text4 = "?";
-$text5 = "Perchè";
-$text6 = $text2;
-$text7 = 'bevuto';
-$text8 = "tutto";
+// $text1 = "Marco";
+// $text2 = "hai";
+// $text3 = "sete";
+// $text4 = "?";
+// $text5 = "Perchè";
+// $text6 = $text2;
+// $text7 = 'bevuto';
+// $text8 = "tutto";
 
-echo $text1 . " " . $text2 . " " . $text3 . $text4 . " " . $text5 . " " . $text6 . " " . $text7 . " " . $text8;
+// echo $text1 . " " . $text2 . " " . $text3 . $text4 . " " . $text5 . " " . $text6 . " " . $text7 . " " . $text8;
 
 
 // fine secondo esercizio
 
+// terzo esercizio
 
 
 
+$words1 = [ 'vostro', 'Nel', 'smarrita','colle','mi','sempre',['oscura','era','di',['mezzo','E'],'ritrovai','per'],'diritta','che'];
+  
+$words2 = ['elemento1' => 'cammin','elemento2' => 'nostra','elemento3' => ['via','vita','una',['selva']],'fine' => 'la'];
 
 
+$results = $words1[1] . " " . $words1[6][3][0] ." " .  $words1[6][2] . " " . $words2['elemento1']. " " .  $words1[6][2] . " " . $words2['elemento2'] . " " . $words2['elemento3'][1] . " " . $words1[4] . " " . $words1[6][4] . " " . $words1[6][5] . " " . $words2['elemento3'][2] . " " . $words2['elemento3'][3][0] . " " . $words1[6][0] . ", " . $words1[8] . " " . $words2['fine'] . " " .  $words1[7] . " " . $words2['elemento3'][0] . " " . $words1[6][1] ." " . $words1[2] . ".";
 
+echo $results;
 
+// fine terzo esercizio
